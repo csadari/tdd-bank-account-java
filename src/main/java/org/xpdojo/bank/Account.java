4643d3ea2c766a07be58841d473e4353e4cf547a
csadari <adarics@gmail.com>
2020-07-16 00:45:12 -0400

    Added Samples Test cases

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,5 +1,38 @@
 package org.xpdojo.bank;
 
-public class Accounat { 
-// Write code here 
+public class Account {
+    private String acctNumber;
+    private double balance;
+
+    public Account(String acctNum, double initialBalance)
+    {
+        acctNumber = acctNum;
+        balance = initialBalance;
+    }
+
+    public void depositAmount( double moneyToBeDeposited) {
+        this.balance= this.balance + moneyToBeDeposited;
+    }
+
+    public void withdrawAmount( double moneyToBeWithdrawn) {
+        this.balance= this.balance - moneyToBeWithdrawn;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+    public void printReport ()
+    {
+        System.out.println("The Account Number is :" + this.acctNumber);
+        System.out.println("The balance is :" +  this.balance);
+    }
+    public static void transferFunds(Account fromAccount, Account toAccount,  double transferAmount)
+    {
+        if (fromAccount.getBalance() >= transferAmount)
+        { fromAccount.withdrawAmount(transferAmount);
+            toAccount.depositAmount(transferAmount);
+        }
+    }
+
 }
